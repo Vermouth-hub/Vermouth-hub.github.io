65e5b217ba2019938d12860a65da0b95b34b804a
Vermouth-hub <2771897630@qq.com>
2026-02-08 15:51:46 +0800

    astro-blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = '';
+export const SITE_TAGLINE = 'We1c0me! (≧◡≦) ';
+export const SITE_DESCRIPTION = 'We1c0me t0 Ve2m0uth\'s blog, have fun here!'; // site meta description for SEO
