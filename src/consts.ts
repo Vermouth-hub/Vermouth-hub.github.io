826e3995485b933ee25d3c7750a49516566dc52b
Vermouth-hub <2771897630@qq.com>
2026-02-14 20:47:51 +0800

    astro-blog

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '';
-export const SITE_TAGLINE = 'We1c0me! (≧◡≦) ';
+export const SITE_TAGLINE = 'We1c0me! (๑• . •๑)';
 export const SITE_DESCRIPTION = 'We1c0me t0 Ve2m0uth\'s blog, have fun here!'; // site meta description for SEO
